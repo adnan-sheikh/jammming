29d62b6ed57f312d2f70d363338bdf517a16dd05
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-12 21:16:52 +0530

    add Track comp and css

---
+++ src/Components/Track/Track.js
@@ -0,0 +1,24 @@
+import "./Track.css";
+import React from "react";
+
+export class Track extends React.Component {
+  renderAction() {
+    if (this.props.isRemoval) {
+      return <button className="Track-action" >-</button>;
+    } else {
+      return <button className="Track-action" >+</button>;
+    }
+  }
+
+  render() {
+    return (
+      <div className="Track">
+        <div className="Track-information">
+          <h3>Track-name</h3>
+          <p>Track-artist | Track-album</p>
+        </div>
+        {this.renderAction()}
+      </div>
+    )
+  }
+}
--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -1,11 +1,12 @@
 import "./TrackList.css";
 import React from "react";
+import { Track } from "../Track/Track";
 
 export class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        
+        <Track />
       </div>
     )
   }
