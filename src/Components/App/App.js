f7d3369d4d310f7c12ae68c74811ddd0d903355d
mech-coder <40724165+adnan-sheikh@users.noreply.github.com>
2021-05-13 18:07:10 +0530

    add addTrack method to add track from SearchResults to Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -31,25 +31,37 @@ class App extends React.Component {
       playlistName: 'Playlist',
       playlistTracks: [
         {
-          name: 'name1', 
-          artist: 'artist1', 
-          album: 'album1', 
-          id: 1
+          name: 'playlistName1', 
+          artist: 'playlistArtist1', 
+          album: 'playlistAlbum1', 
+          id: 4
         },
         {
-          name: 'name2', 
-          artist: 'artist2', 
-          album: 'album2', 
-          id: 2
+          name: 'playlistName2', 
+          artist: 'playlistArtist2', 
+          album: 'playlistAlbum2',
+          id: 5
         },
         {
-          name: 'name3', 
-          artist: 'artist3', 
-          album: 'album3', 
-          id: 3
+          name: 'playlistName3', 
+          artist: 'playlistArtist3', 
+          album: 'playlistAlbum3',
+          id: 6
         }
       ]
     };
+    this.addTrack = this.addTrack.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => {
+      return savedTrack.id === track.id;
+    })) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({playlistTracks: tracks});
   }
 
   render() {
@@ -61,7 +73,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            <SearchResults searchResults={this.state.searchResults} />
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} />
           </div>
         </div>
